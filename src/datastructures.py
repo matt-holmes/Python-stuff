8cbfa7a3c8cbac284c13be0d2c0c7ca03fafad2d
Matt Holmes <mattholmes@live.com>
2017-08-19 19:02:30 -0500

    adding some more stuffs

--- src/datastructures.py
+++ src/datastructures.py
@@ -2,8 +2,25 @@ class Datastructures():
 
     dict = {'Jon':['Jon','smith',18], 'Sam':['Sam', 'Green', 56]}
 
-    def get_age_from_dict(self, name):
+    list = ['jon', 'alex', 'sam', 'jane']
+
+    def get_age_from_name_from_dict(self, name):
         if(name in self.dict):
             return self.dict[name][2]
         else:
             return None
+
+    def create_new_list_upper(self):
+        newList = [];
+        for item in self.list:
+            newList.append(item.upper())
+        return newList
+
+    def create_new_dict_upper_last_name(self):
+        newDict = {}
+        for key, val in self.dict.iteritems():
+            newDict[key] = [val[0], val[1].upper(), val[2]]
+        return newDict
+
+    def get_list(self):
+        return self.list
--- src/tests/testDatastructures.py
+++ src/tests/testDatastructures.py
@@ -6,14 +6,36 @@ from src import datastructures
 
 class TestDatastructures(unittest.TestCase):
 
+    inst = datastructures.Datastructures()
+
     def test_get_age_from_name_from_dictionary(self):
-        inst = datastructures.Datastructures()
-        self.assertEqual(inst.get_age_from_dict('Jon'), 18)
-        self.assertEqual(inst.get_age_from_dict('Sam'), 56)
+        self.assertEqual(self.inst.get_age_from_name_from_dict('Jon'), 18)
+        self.assertEqual(self.inst.get_age_from_name_from_dict('Sam'), 56)
 
     def test_dictionary_key_doesnt_exist(self):
-        inst = datastructures.Datastructures()
-        self.assertEqual(inst.get_age_from_dict('Alex'), None)
+        self.assertEqual(self.inst.get_age_from_name_from_dict('Alex'), None)
+
+    def test_list_append(self):
+        self.inst.get_list().append('billy')
+        self.assertEqual(len(self.inst.get_list()), 5)
+
+    def test_list_pop(self):
+        self.inst.get_list().pop()
+        self.assertEqual(len(self.inst.get_list()), 4)
+        self.assertEqual(self.inst.get_list()[2], 'sam')
+
+    def test_create_new_list_upper(self):
+        a_list = self.inst.create_new_list_upper()
+        self.assertEqual(a_list[0], 'JON')
+        self.assertEqual(a_list[1], 'ALEX')
+        self.assertEqual(a_list[2], 'SAM')
+        self.assertEqual(a_list[3], 'JANE')
+
+    def test_create_new_dict_upper_last_name(self):
+        a_dict = self.inst.create_new_dict_upper_last_name()
+        self.assertEqual(a_dict['Jon'][1], 'SMITH')
+        self.assertEqual(a_dict['Sam'][1], 'GREEN')
+
 
 if __name__ == '__main__':
     unittest.main()
